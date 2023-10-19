0c6bfaa0953ed538e058dccb30fddc8134889f6b
taiane94sb <taiane19sb94@gmail.com>
2023-10-19 10:13:30 -0300

    estrutura de classe

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo da classe
+ */
+public class HelloWorld {
+    // Todo conteúdo deverá ser inserido aqui dentro da classe
+    
+}
