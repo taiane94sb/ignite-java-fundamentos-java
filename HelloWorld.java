d3f630ec3f48407b687aac081faaa16878c72348
taiane94sb <taiane19sb94@gmail.com>
2023-10-20 10:20:59 -0300

    tipo de dados

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,28 @@
 public class HelloWorld {
     // Todo conteúdo deverá ser inserido aqui dentro da classe
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /**
+         * Valores (int, double, float, long)
+         * Texto (String)
+         * Booleano (boolean)
+         */
+
+         System.out.println("Valores (int, double, float, long)");
+         int dadoDoTipoInt = 10;
+         System.out.println(dadoDoTipoInt);
+         double dadoDoTipoDouble = 3.14;
+         System.out.println(dadoDoTipoDouble);
+         float dadoDoTipoFloat = 3.14f;
+         System.out.println(dadoDoTipoFloat);
+         long dadoDoTipoLong = 123456789L;
+         System.out.println(dadoDoTipoLong);
+
+         System.out.println("Texto (String)");
+         String dadoDoTipoString = "Taiane";
+         System.out.println(dadoDoTipoString);
+
+         System.out.println("Booleano (boolean)");
+         boolean dadoDoTipoBoolean = true;
+         System.out.println(dadoDoTipoBoolean);
     }
-    
 }
