ff33f550062279f678e07e118c62d423c057836e
taiane94sb <taiane19sb94@gmail.com>
2023-10-23 17:56:00 -0300

    wrapper classes

---
+++ wrapperClasses/WrapperClasses.java
@@ -0,0 +1,21 @@
+package wrapperClasses;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1.0f;
+        boolean tipoBoolean = false;
+
+        Integer tipoInt2 = 1;
+        Float tipoFloat2 = 1.0F;
+        Boolean tipoBoolean2 = false;
+        
+        System.out.println(tipoInt);
+        System.out.println(tipoFloat);
+        System.out.println(tipoBoolean);
+
+        System.out.println(tipoInt2);
+        System.out.println(tipoFloat2);
+        System.out.println(tipoBoolean2);
+    }
+}
