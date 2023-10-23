c26db45cc39db05e60d0b82e84559a72a0e53fcd
taiane94sb <taiane19sb94@gmail.com>
2023-10-23 15:44:59 -0300

    herança

--- pessoa/Pessoa.java
+++ pessoa/Pessoa.java
@@ -1,9 +1,33 @@
 package pessoa;
 
 public class Pessoa {
-    String nome;
-    int idade;
-    String cpf;
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
     String imprimirDadosDaPessoa() {
         return "O nome da pessoa é: " + nome + ", a sua idade é: " + idade + " e o seu cpf é: " + cpf + ".";
--- pessoa/TestePessoa.java
+++ pessoa/TestePessoa.java
@@ -3,25 +3,39 @@ package pessoa;
 public class TestePessoa {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa();
-        pessoa1.cpf = "12345678912";
-        pessoa1.nome = "Taiane";
-        pessoa1.idade = 29;
+        pessoa1.setCpf("12345678912");
+        pessoa1.setNome("Taiane");
+        pessoa1.setIdade(29);
 
         System.out.println(pessoa1);
-        System.out.println(pessoa1.cpf);
-        System.out.println(pessoa1.nome);
-        System.out.println(pessoa1.idade);
+        System.out.println(pessoa1.getCpf());
+        System.out.println(pessoa1.getNome());
+        System.out.println(pessoa1.getIdade());
         System.out.println(pessoa1.imprimirDadosDaPessoa());
 
         Pessoa pessoa2 = new Pessoa();
-        pessoa2.cpf = "78945612378";
-        pessoa2.nome = "Matheus";
-        pessoa2.idade = 25;
+        pessoa2.setCpf("78945612378");
+        pessoa2.setNome("Matheus");
+        pessoa2.setIdade(25);
 
         System.out.println(pessoa2);
-        System.out.println(pessoa2.cpf);
-        System.out.println(pessoa2.nome);
-        System.out.println(pessoa2.idade);
+        System.out.println(pessoa2.getCpf());
+        System.out.println(pessoa2.getNome());
+        System.out.println(pessoa2.getIdade());
         System.out.println(pessoa2.imprimirDadosDaPessoa());
+
+        Professor professor1 = new Professor();
+        professor1.setNome("Ana Maria");
+        professor1.setIdade(30);
+        professor1.setCpf("11111111111");
+        professor1.setSalario(6000);
+        System.out.println(professor1.imprimirDadosDaPessoa());
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Ana Clara");
+        aluno1.setIdade(1530);
+        aluno1.setCpf("22222222222");
+        aluno1.setMatricula("123456123456");
+        System.out.println(aluno1.imprimirDadosDaPessoa());
     }
 }
